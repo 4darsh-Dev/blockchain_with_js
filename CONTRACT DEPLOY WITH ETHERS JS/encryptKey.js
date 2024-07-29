a26c1445e86391807bee8d06e4b6b2e01e00ac9b
adarsh <adarshmaurya9415@gmail.com>
2024-07-29 19:42:55 +0530

    encrypted key with pass setup

---
+++ CONTRACT DEPLOY WITH ETHERS JS/encryptKey.js
@@ -0,0 +1,25 @@
+const ethers = require("ethers")
+const fs = require("fs-extra")
+require("dotenv").config()
+
+async function main() {
+    const wallet = new ethers.Wallet(process.env.PRIVATE_KEY)
+
+    const encryptedJsonKey = await wallet.encrypt(
+        process.env.PRIVATE_KEY_PASSWORD
+    )
+    // In later version (^6.2.3 as of this commit) of etherjs, PRIVATE_KEY is inferred from wallet, so there is no need to
+    // pass private key again.
+    //     const encryptedJsonKey = await wallet.encrypt(
+    //         process.env.PRIVATE_KEY_PASSWORD,
+    //  )
+    console.log(encryptedJsonKey)
+    fs.writeFileSync("./.encryptedKey.json", encryptedJsonKey)
+}
+
+main()
+    .then(() => process.exit(0))
+    .catch((error) => {
+        console.error(error)
+        process.exit(1)
+    })
